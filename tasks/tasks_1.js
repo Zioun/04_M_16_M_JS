0ee4699ec6262863a06025f62d141aa6ab7c31ef
Zioun <jionkhan0@gmail.com>
2024-01-26 21:57:13 +0600

    Task 1 Complete

---
+++ tasks/tasks_1.js
@@ -0,0 +1,15 @@
+var amount = 1000;
+console.log('Amount'+' '+amount+' '+'Taka');
+
+var orange = 250;
+console.log('Orange'+' '+orange+' '+'Taka');
+var apple = 300;
+console.log('Apple'+' '+apple+' '+'Taka');
+
+var buy = orange+apple;
+console.log('Buy'+' '+buy);
+
+var returnTaka = amount-buy;
+console.log('Return'+' '+returnTaka);
+
+
