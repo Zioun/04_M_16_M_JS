3db37df2c2e0a6849f35dfb8379e119c09710a7c
Zioun <jionkhan0@gmail.com>
2024-01-26 21:21:29 +0600

    added all

--- number.js
+++ number.js
@@ -1,6 +1,15 @@
-var number1 = 123.456;
-var number2 = 5;
+// var number1 = 123.456;
+// var number2 = 5;
 
-var total = number1+number2;
+// var total = number1+number2;
+// var convert = total.toFixed(0);
+// var convertToSt = parseInt(convert);
 
-console.log(total.toFixed(0))
+// console.log(convertToSt);
+
+
+
+var numb1 = '0.1';
+var numb2 = 2;
+var convert = parseFloat(numb1)
+console.log(convert+numb2)
